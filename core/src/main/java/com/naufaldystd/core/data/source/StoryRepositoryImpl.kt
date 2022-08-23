1c576353d9049aa11cc0701e9cc14611bd6fbc19
blitzkz23 <blitzaldykazu@gmail.com>
2022-08-23 09:43:05 +0700

    feat: setup room db boilerplate-related files

--- core/src/main/java/com/naufaldystd/core/data/source/StoryRepositoryImpl.kt
+++ core/src/main/java/com/naufaldystd/core/data/source/StoryRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.naufaldystd.core.data.source
 
+import com.naufaldystd.core.data.source.local.LocalDataSource
 import com.naufaldystd.core.data.source.remote.RemoteDataSource
 import com.naufaldystd.core.domain.repository.StoryRepository
 import com.naufaldystd.core.utils.AppExecutors
@@ -9,6 +10,7 @@ import javax.inject.Singleton
 @Singleton
 class StoryRepositoryImpl @Inject constructor(
 	private val remoteDataSource: RemoteDataSource,
+	private val localDataSource: LocalDataSource,
 	private val appExecutors: AppExecutors
 ) : StoryRepository {
 
