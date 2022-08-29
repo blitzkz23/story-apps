bbb0fbaacabc88027e4b09a4e666e86eb72861ab
blitzkz23 <blitzaldykazu@gmail.com>
2022-08-29 12:36:39 +0700

    feat: prototyping temporary login page and styling such as font, more color, etc.

--- app/src/main/java/com/naufaldystd/storyapps/ui/login/LoginFragment.kt
+++ app/src/main/java/com/naufaldystd/storyapps/ui/login/LoginFragment.kt
@@ -5,16 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.naufaldystd.storyapps.R
+import com.naufaldystd.storyapps.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+	private var _binding : FragmentLoginBinding? = null
+
+	// This property is only valid between onCreateView and
+	// onDestroyView.
+	private val binding get() = _binding!!
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
-	): View? {
+	): View {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_login, container, false)
+		_binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		binding.ctaRegister.setOnClickListener {
+			findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+		}
 	}
 
 
--- app/src/main/java/com/naufaldystd/storyapps/ui/main/MainActivity.kt
+++ app/src/main/java/com/naufaldystd/storyapps/ui/main/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.naufaldystd.storyapps.ui.main
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -21,7 +24,21 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
 
+		setupFullscreen()
 		setupViewModel()
+	}
+
+	@Suppress("DEPRECATION")
+	private fun setupFullscreen() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+			window.insetsController?.hide(WindowInsets.Type.statusBars())
+		} else {
+			window.setFlags(
+				WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN
+			)
+		}
+		supportActionBar?.hide()
 	}
 
 	private fun setupViewModel() {
