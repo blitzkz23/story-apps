917907cd05ae8c9bd2a336fcaa0a2daefffe2853
blitzkz23 <blitzaldykazu@gmail.com>
2022-09-02 09:29:39 +0700

    feat: implement camera x functionality successfully

--- app/src/main/java/com/naufaldystd/storyapps/ui/story/add/CameraXActivity.kt
+++ app/src/main/java/com/naufaldystd/storyapps/ui/story/add/CameraXActivity.kt
@@ -1,13 +1,20 @@
 package com.naufaldystd.storyapps.ui.story.add
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.naufaldystd.storyapps.databinding.ActivityCameraXactivityBinding
+import com.naufaldystd.storyapps.utils.createFile
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -52,11 +59,67 @@ class CameraXActivity : AppCompatActivity() {
 	}
 
 	private fun takePhoto() {
+		val imageCapture = imageCapture ?: return
 
+		val photoFile = createFile(application)
+
+		val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+		imageCapture.takePicture(
+			outputOptions,
+			ContextCompat.getMainExecutor(this),
+			object : ImageCapture.OnImageSavedCallback {
+				override fun onError(exc: ImageCaptureException) {
+					Toast.makeText(
+						this@CameraXActivity,
+						"Gagal mengambil gambar.",
+						Toast.LENGTH_SHORT
+					).show()
+				}
+
+				override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+					val intent = Intent()
+					intent.putExtra("picture", photoFile)
+					intent.putExtra(
+						"isBackCamera",
+						cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA
+					)
+					setResult(AddStoryActivity.CAMERA_X_RESULT, intent)
+					finish()
+				}
+			}
+		)
 	}
 
 	private fun startCamera() {
+		val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
+		cameraProviderFuture.addListener({
+			val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+			val preview = Preview.Builder()
+				.build()
+				.also {
+					it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+				}
+
+			imageCapture = ImageCapture.Builder().build()
+
+			try {
+				cameraProvider.unbindAll()
+				cameraProvider.bindToLifecycle(
+					this,
+					cameraSelector,
+					preview,
+					imageCapture
+				)
+
+			} catch (exc: Exception) {
+				Toast.makeText(
+					this@CameraXActivity,
+					"Gagal memunculkan kamera.",
+					Toast.LENGTH_SHORT
+				).show()
+			}
+		}, ContextCompat.getMainExecutor(this))
 	}
 
 	private fun hideSystemUI() {
